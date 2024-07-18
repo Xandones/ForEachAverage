6dd5c97f2dcf547fdbb1a6d737a2947d7b4c8b7d
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2024-07-18 19:14:14 -0300

    Add project files.

---
+++ ForEachAverage.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	// In this exercise we have some values for speed of a vehicle in km/h (an array) and we want to calculate the average of them using 'foreach'.
+	float RecordedSpeeds[] {70.9f, 89.5f, 65.6f, 102.5f, 123.6f, 63.4f, 74.5f, 99.5f, 68.7f};
+	float AverageSpeed = 0.0;
+	int Size = sizeof(RecordedSpeeds) / sizeof(RecordedSpeeds[0]);
+
+	for (auto x : RecordedSpeeds)
+		AverageSpeed += x;
+
+	AverageSpeed /= Size;
+	printf("The average speed is %.2f km/h.", AverageSpeed);
+}
