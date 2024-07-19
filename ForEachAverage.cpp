152e03cf0d5fad5f76d64aee8ec80c85f3f64a40
Alexandro H. Lopes <49080728+Xandones@users.noreply.github.com>
2024-07-19 18:30:56 -0300

    Fixed comment
    There were some errors in the comment. They were fixed.

--- ForEachAverage.cpp
+++ ForEachAverage.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	// In this exercise we have some values for speed of a vehicle in km/h (an array) and we want to calculate the average of them using 'foreach'.
+	// In this exercise we have some values for the speeds of vehicles in km/h (inside an array) and we want to calculate their average using 'foreach'.
 	float RecordedSpeeds[] {70.9f, 89.5f, 65.6f, 102.5f, 123.6f, 63.4f, 74.5f, 99.5f, 68.7f};
 	float AverageSpeed = 0.0;
 	int Size = sizeof(RecordedSpeeds) / sizeof(RecordedSpeeds[0]);
